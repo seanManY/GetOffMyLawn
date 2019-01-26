4947c9fac0ab711c2157b35e4d10ac3577d81dd7
Sean Maguire <maguire.sean037@gmail.com>
2019-01-26 04:50:03 -0800

    made player a prefab

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GunScript : MonoBehaviour
+
+    
 {
+    public float damage = 10;
+    public float range = 100f;
+
+    public Camera fpsCam;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,20 @@ public class GunScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+
+        }
+
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
     }
 }
