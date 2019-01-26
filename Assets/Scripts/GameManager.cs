483b9d80425c83f38c942696dfd8f171dc0208ae
Sean Maguire <maguire.sean037@gmail.com>
2019-01-26 01:14:49 -0800

    you on the same page boo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject lane1Spawn;
+    public GameObject lane2Spawn;
+    public GameObject lane3Spawn;
+    public GameObject lane4Spawn;
+    public GameObject lane5Spawn;
+    public GameObject lane6Spawn;
+    public GameObject lane7Spawn;
+    public GameObject lane8Spawn;
+    public GameObject lane9Spawn;
+
+    public GameObject basicEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
