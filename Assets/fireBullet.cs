b4fe028c62544b9a68538edb0e0f1e3f615c1517
reSLANE <sml37s@yahoo.com>
2019-01-26 02:53:11 -0800

    Bombs Away

---
+++ Assets/fireBullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireBullet : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform parent;
+    public int trigger = 10;
+    int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = trigger/2;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        count++;
+        if(count >= trigger)
+        {
+            Instantiate(bullet, parent);
+            count = 0;
+        }
+    }
+}
---
+++ Assets/moveBullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBullet : MonoBehaviour
+{
+    public float speed = 1f;
+    int trigger;
+    int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trigger = 600;
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        count++;
+        if(count >= trigger)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
