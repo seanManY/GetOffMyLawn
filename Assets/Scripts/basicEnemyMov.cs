eab1f090e46f96ed974b22994042715543bc2242
Sean Maguire <maguire.sean037@gmail.com>
2019-01-26 00:06:50 -0800

    enemy
    chuga chuga choo bitch

---
+++ Assets/Scripts/basicEnemyMov.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basicEnemyMov : MonoBehaviour
+{
+
+    public float speed = 1f;
+
+    
+
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
+    }
+}
